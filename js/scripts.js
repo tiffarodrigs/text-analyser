44c669bfac6453bae549c1f72e2c8e50d670875e
Tiffany Rodrigo <tiffa.rodrigs@gmail.com>
2022-01-19 17:35:23 -0800

    Add code for top three most used words

--- js/scripts.js
+++ js/scripts.js
@@ -75,9 +75,114 @@ return htmlStringOffensive + "</p>";
 
 }
 
-function threeMostUsedWord(text){
-let textArray = text.split("");
+
+
+/*function threeMostUsedWord(text) {
+  let textArray = text.split(" ");
+let sortArray = textArray.sort();
+var current = null;
+var cnt = 0;
+for (var i = 0; i < sortArray.length; i++) {
+    if (sortArray[i] != current) {
+        if (cnt > 0) {
+            console.log(current + ' comes --> ' + cnt + ' times<br>');
+        }
+        current = sortArray[i];
+        cnt = 1;
+    } else {
+        cnt++;
+    }
 }
+if (cnt > 0) {
+    console.log(current + ' comes --> ' + cnt + ' times');
+}
+
+}*/
+//hi hi
+//hi hello
+
+// 1. Split the sentence
+// 2. Iterate the array
+// 3. if key exists increase the counter.
+
+function getTopThree(input) {
+  // return input.split(" ").reduce(function (result, value) {
+  //   if(result[value]) {
+  //     result[value] = result[value] + 1;
+  //   } else {
+  //     result[value] = 1;
+  //   }
+  //   return result;
+  // }, {})
+
+  const array = input.split(" ");
+  const sortedArray = array.sort();
+  let currWord;
+  let counter;
+  const wordCounter = [];
+ // let finalArray=[];
+  sortedArray.forEach(function (value) {
+    if(currWord !== value){
+      if(currWord !== undefined)
+      {
+        wordCounter.push([currWord,counter]);
+      }
+      currWord = value;
+      counter = 1;
+    } else {
+      counter = counter + 1;
+    }
+  })
+  wordCounter.push([currWord,counter]);
+ wordCounter.sort( function(a,b) { return a[1] - b[1]})
+  finalArray = wordCounter.splice(0, 3);
+  console.log(finalArray);
+
+}
+
+
+function threeMostUsedWord(text){
+  let textArray = text.split(" ");
+  let count=0;
+  let matchedWord=null;
+  let resultArray=[];
+  const wordCounter = [];
+  let finalArray=[];
+  textArray.sort().forEach(function(element)
+  {
+    if(element!=matchedWord)
+    {
+      if(count>0)
+      {
+        count++;
+      }
+      matchedWord=element;
+      count=1;
+
+     // console.log(matchedWord + ' comesi --> ' + count + ' times');
+    }
+    else
+    {
+      count++;
+     // console.log(matchedWord + ' comes --> ' + count + ' times');
+
+    }
+    wordCounter.push([matchedWord,count]);
+    //console.log(wordCounter);
+    wordCounter.sort( function(a,b) { return b[1] - a[1]});
+
+  });
+
+   finalArray = wordCounter.splice(0, 3);
+   finalArray.forEach(function(element){
+     console.log(element);
+     //do string manipulation
+
+   });
+   
+
+
+
 
 
 
